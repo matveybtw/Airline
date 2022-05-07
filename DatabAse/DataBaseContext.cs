a65cac6b13778f83d28b40c891cee27e6f835807
matveybtw <mat20061119@gmail.com>
2022-05-07 10:13:40 +0300

    Create Database

---
+++ DatabAse/DataBaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace DataBase
+{
+    public class DataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=91.238.103.51;Port=5743;Database=dbirabv012;User Id=bv012irauser;Password=Wds543ieiidijYYdjsi**jdjkUDJEKIDdhYiiskdUUdskkYJDJDNNnndmd73d@sdf88U^ew!;");
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
+    }
+}
